a551fb17a0bf96183706d3f53e6ca59e3fd2c155
gbaliarda <gbaliarda@itba.edu.ar>
2023-05-10 16:12:36 -0300

    feat: beeman evolution
    Co-authored-by: EperezOk <eperez@itba.edu.ar>

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -39,29 +39,13 @@ public class App {
         double k = Math.pow(10, 4); // N/m
         double gamma = 100; // kg/s
         double A = 1; // m
-        double tf = 5; // s
 
         // Initial Conditions
         double x = 1;
         double v = -A*gamma / (2*m);
 
-        double t = 0;
-
-        double prevX = DampedOscillator.EulerIntegral(x, v, -k*x - gamma*v, -dt, m);
-
-        for (int i = 0; t < tf; i++) {
-            double f = -k*x - gamma * v;
-            double auxX = x;
-
-            x = DampedOscillator.VerletIntegral(x, f, prevX, dt, m);
-            v = (x - prevX) / (2*dt);
-
-            if (Config.isVerbose()) System.out.printf("t=%.2f -> x=%.2f ; v=%.2f\n", t, x, v);
-            if (i % Config.getOutputInterval() == 0) outputWriter.write(String.format("%.2f\n%.4f %.4f\n", t, x, v));
-
-            prevX = auxX;
-            t += dt;
-        }
+//        DampedOscillator.VerletEvolution(outputWriter, x, v, k, gamma, dt, m);
+        DampedOscillator.BeemanEvolution(outputWriter, x, v, k, gamma, dt, m);
     }
 
     private static void exercise2(FileWriter outputWriter, double dt) throws IOException {
--- src/main/java/DampedOscillator.java
+++ src/main/java/DampedOscillator.java
@@ -1,3 +1,7 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Locale;
+
 public class DampedOscillator {
 
     public static double GearPredictorCorrectorIntegral(double x, double v, double a, double dt, double m, double k, double gamma) {
@@ -31,16 +35,27 @@ public class DampedOscillator {
         return xc;
     }
 
-    public static double BeemanIntegral(double x, double v, double a, double prev_a, double dt, double m) {
-        return x + v*dt + (2.0/3)*a*Math.pow(dt, 2) - (1.0/6)*prev_a*Math.pow(dt, 2);
+    public static double BeemanPosition(double x, double v, double a, double prevA, double dt) {
+        return x + v*dt + (2.0/3)*a*Math.pow(dt, 2) - (1.0/6)*prevA*Math.pow(dt, 2);
+    }
+
+    public static double BeemanVelocity(double nextX, double v, double a, double prevA, double dt, double m, double gamma, double k) {
+//        return v + (1.0/3)*next_a*dt + (5.0/6)*a*dt - (1.0/6)*prev_a*dt;
+        double denominator = 1 + (dt * gamma) / (3*m);
+        double numerator = v - (dt * k * nextX)/(3*m) + (5.0/6)*a*dt - (1.0/6)*prevA*dt;
+        return numerator/denominator;
     }
 
     public static double VerletIntegral(double x, double f, double prevX, double dt, double m) {
         return 2*x - prevX + (Math.pow(dt, 2) / m) * f;
     }
 
-    public static double EulerIntegral(double x, double v, double f, double dt, double m) {
+    public static double EulerPosition(double x, double v, double f, double dt, double m) {
         return x + dt*v + (Math.pow(dt, 2) / (2*m)) * f;
+    }
+
+    public static double EulerVelocity(double v, double f, double dt, double m) {
+        return v + (dt / m) * f;
     }
 
     private static int factorial(int n) {
@@ -49,4 +64,45 @@ public class DampedOscillator {
         return n * factorial(n-1);
     }
 
+    public static void VerletEvolution(FileWriter outputWriter, double x, double v, double k, double gamma, double dt, double m) throws IOException {
+        double t = 0, tf = 5;
+
+        double prevX = DampedOscillator.EulerPosition(x, v, -k*x - gamma*v, -dt, m);
+
+        for (int i = 0; t < tf; i++) {
+            double f = -k*x - gamma * v;
+            double auxX = x;
+
+            x = DampedOscillator.VerletIntegral(x, f, prevX, dt, m);
+            v = (x - prevX) / (2*dt);
+
+            if (Config.isVerbose()) System.out.printf("t=%.2f -> x=%.2f ; v=%.2f\n", t, x, v);
+            if (i % Config.getOutputInterval() == 0) outputWriter.write(String.format(Locale.US, "%.2f\n%.4f %.4f\n", t, x, v));
+
+            prevX = auxX;
+            t += dt;
+        }
+    }
+
+    public static void BeemanEvolution(FileWriter outputWriter, double x, double v, double k, double gamma, double dt, double m) throws IOException {
+        double t = 0, tf = 5;
+        double f = -k*x - gamma*v;
+        double prevX = DampedOscillator.EulerPosition(x, v, f, -dt, m);
+        double prevV = DampedOscillator.EulerVelocity(v, f, -dt, m);
+        double prevA = (-k*prevX - gamma*prevV)/m;
+
+        for (int i = 0; t < tf; i++) {
+            f = -k*x - gamma * v;
+            double a = f/m;
+
+            x = DampedOscillator.BeemanPosition(x, v, a, prevA, dt);
+            v = DampedOscillator.BeemanVelocity(x, v, a, prevA, dt, m, gamma, k);
+
+            if (Config.isVerbose()) System.out.printf("t=%.2f -> x=%.2f ; v=%.2f\n", t, x, v);
+            if (i % Config.getOutputInterval() == 0) outputWriter.write(String.format(Locale.US, "%.2f\n%.4f %.4f\n", t, x, v));
+
+            prevA = a;
+            t += dt;
+        }
+    }
 }

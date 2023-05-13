792aa4487b76ebc4f2434de432bc5c935c2b5c1b
gbaliarda <gbaliarda@itba.edu.ar>
2023-05-12 23:57:20 -0300

    feat: gear in pool billiard

--- src/main/java/CollisionSystem.java
+++ src/main/java/CollisionSystem.java
@@ -12,26 +12,93 @@ public class CollisionSystem {
         this.dt = dt;
     }
 
-    public void nextEvent() {
-        // Update each particle position and velocity
-        for (Particle p : particles) {
-            if (p.isFixed()) continue;
+    private double[] getPredictions(double r, double r1, double r2, double r3, double r4, double r5) {
+        double rp = r + r1 * dt + r2 * Math.pow(dt, 2) / factorial(2) + r3 * Math.pow(dt, 3) / factorial(3) + r4 * Math.pow(dt, 4) / factorial(4) + r5 * Math.pow(dt, 5) / factorial(5);
+        double r1p = r1 + r2 * dt + r3 * Math.pow(dt, 2) / factorial(2) + r4 * Math.pow(dt, 3) / factorial(3) + r5 * Math.pow(dt, 4) / factorial(4);
+        double r2p = r2 + r3 * dt + r4 * Math.pow(dt, 2) / factorial(2) + r5 * Math.pow(dt, 3) / factorial(3);
+        double r3p = r3 + r4 * dt + r5 * Math.pow(dt, 2) / factorial(2);
+        double r4p = r4 + r5 * dt;
+        double r5p = r5;
 
-            double newX = nextPosition(p.getX(), p.getVx(), p.getFx(), p.getMass(), p.getPrevX());
-            double newY = nextPosition(p.getY(), p.getVy(), p.getFy(), p.getMass(), p.getPrevY());
-            double newVx = nextVelocity(newX, p.getPrevX(), p.getVx(), p.getFx(), p.getMass());
-            double newVy = nextVelocity(newY, p.getPrevY(), p.getVy(), p.getFy(), p.getMass());
-            p.setX(newX);
-            p.setY(newY);
-            p.setVx(newVx);
-            p.setVy(newVy);
+        return new double[]{rp, r1p, r2p, r3p, r4p, r5p};
+    }
+
+    private List<Particle> gearNextState() {
+        // Create new list of particles because each particle needs to be predicted and corrected
+        // with the original particles. e.g. Particle 1 predicts in t+dt, check forces
+        // with particle 2 with t values and correct particle 1 and save it. Then particle 2 does the same with
+        // particle 1 with t values.
+        List<Particle> newParticles = new ArrayList<>();
+        for (Particle p : particles) {
+            if (p.isFixed()) {
+                newParticles.add(p);
+                continue;
+            }
+            Particle newParticle = new Particle(p.getX(), p.getY(), p.getVx(), p.getVy(), p.getFx(), p.getFy(), p.getMass(), p.getRadius(), p.getColor(), p.getX2(), p.getX3(), p.getX4(), p.getX5(), p.getY2(), p.getY3(), p.getY4(), p.getY5());
+            // Predict new particle derivatives
+            double[] xPredicts = getPredictions(newParticle.getX(), p.getVx(), p.getX2(), p.getX3(), p.getX4(), p.getX5());
+            double[] yPredicts = getPredictions(newParticle.getY(), p.getVy(), p.getY2(), p.getY3(), p.getY4(), p.getY5());
+
+            // Calculate a(t+dt) using particle p with predicted values in t+dt and the rest in time t
+            newParticle.setX(xPredicts[0]);
+            newParticle.setY(yPredicts[0]);
+            newParticle.setVx(xPredicts[1]);
+            newParticle.setVy(yPredicts[1]);
+            Set<Particle> toRemove = new HashSet<>();
+            double[] forces = getCollisionForce(newParticle, particles.stream().filter(o -> !o.equals(p)).collect(Collectors.toList()), toRemove);
+            newParticle.setFx(forces[0]);
+            newParticle.setFy(forces[1]);
+            double deltaAx = newParticle.getFx()/newParticle.getMass() - xPredicts[2];
+            double deltaAy = newParticle.getFy()/newParticle.getMass() - yPredicts[2];
+            double deltaR2x = deltaAx * Math.pow(dt, 2) / factorial(2);
+            double deltaR2y = deltaAy * Math.pow(dt, 2) / factorial(2);
+
+            // Correct X and Y, note that this time f only depends on r so first coefficient is 3/20
+            double[] gearCoefficients = {3/20.0, 251/360.0, 1, 11/18.0, 1/6.0, 1/60.0};
+            newParticle.setX(xPredicts[0] + gearCoefficients[0] * deltaR2x);
+            newParticle.setVx(xPredicts[1] + gearCoefficients[1] * deltaR2x / dt);
+            newParticle.setX2(xPredicts[2] + gearCoefficients[2] * deltaR2x * factorial(2) / Math.pow(dt, 2));
+            newParticle.setX3(xPredicts[3] + gearCoefficients[3] * deltaR2x * factorial(3) / Math.pow(dt, 3));
+            newParticle.setX4(xPredicts[4] + gearCoefficients[4] * deltaR2x * factorial(4) / Math.pow(dt, 4));
+            newParticle.setX5(xPredicts[5] + gearCoefficients[5] * deltaR2x * factorial(5) / Math.pow(dt, 5));
+            newParticle.setY(yPredicts[0] + gearCoefficients[0] * deltaR2y);
+            newParticle.setVy(yPredicts[1] + gearCoefficients[1] * deltaR2y / dt);
+            newParticle.setY2(yPredicts[2] + gearCoefficients[2] * deltaR2y * factorial(2) / Math.pow(dt, 2));
+            newParticle.setY3(yPredicts[3] + gearCoefficients[3] * deltaR2y * factorial(3) / Math.pow(dt, 3));
+            newParticle.setY4(yPredicts[4] + gearCoefficients[4] * deltaR2y * factorial(4) / Math.pow(dt, 4));
+            newParticle.setY5(yPredicts[5] + gearCoefficients[5] * deltaR2y * factorial(5) / Math.pow(dt, 5));
+
+            newParticles.add(newParticle);
+        }
+
+        return newParticles;
+    }
+
+    public void nextEvent() {
+
+        // Update each particle position and velocity
+        if (Config.getIntegration().equals("gear"))
+            particles = gearNextState();
+        else {
+            for (Particle p : particles) {
+                if (p.isFixed()) continue;
+
+                double newX = nextPosition(p.getX(), p.getVx(), p.getFx(), p.getMass(), p.getPrevX());
+                double newY = nextPosition(p.getY(), p.getVy(), p.getFy(), p.getMass(), p.getPrevY());
+                double newVx = nextVelocity(newX, p.getPrevX(), p.getVx(), p.getFx(), p.getMass());
+                double newVy = nextVelocity(newY, p.getPrevY(), p.getVy(), p.getFy(), p.getMass());
+                p.setX(newX);
+                p.setY(newY);
+                p.setVx(newVx);
+                p.setVy(newVy);
+            }
         }
 
         Set<Particle> toRemove = new HashSet<>();
 
         // Update forces on each particle
         for (Particle p : particles) {
-            double[] force = getCollisionForce(p, toRemove);
+            double[] force = getCollisionForce(p, particles, toRemove);
             p.setFx(force[0]);
             p.setFy(force[1]);
         }
@@ -49,8 +116,6 @@ public class CollisionSystem {
                 return Integrals.EulerPosition(r, v, f, dt, mass);
             case "verlet":
                 return Integrals.VerletPosition(r, prevR, dt, mass, f);
-            case "gear":
-                return 0;
             default:
                 throw new RuntimeException("Invalid integration method");
         }
@@ -67,7 +132,7 @@ public class CollisionSystem {
         }
     }
 
-    private double[] getCollisionForce(Particle p, Set<Particle> toRemove) {
+    private double[] getCollisionForce(Particle p, List<Particle> particles, Set<Particle> toRemove) {
         double[] force = {0, 0};    // {fx, fy}
         double K = Math.pow(10, 7); // g/s^2
 
@@ -125,5 +190,11 @@ public class CollisionSystem {
 
     public List<Particle> getParticles() {
         return particles;
+    }
+
+    private static int factorial(int n) {
+        if (n == 0)
+            return 1;
+        return n * factorial(n-1);
     }
 }
--- src/main/java/Particle.java
+++ src/main/java/Particle.java
@@ -5,6 +5,16 @@ public class Particle {
     private double prevX = 0, prevY = 0;
     private final String color;
 
+    // Only for gear
+    private double x2;
+    private double x3;
+    private double x4;
+    private double x5;
+    private double y2;
+    private double y3;
+    private double y4;
+    private double y5;
+
     public Particle(double x, double y, double vx, double vy, double fx, double fy, double mass, double radius, String color) {
         this.x = x;
         this.y = y;
@@ -15,7 +25,38 @@ public class Particle {
         this.mass = mass;
         this.radius = radius;
         this.color = color;
+
+        // Only for gear, no particles are close to each other so all x and y from 2 to 5 are 0
+        this.x2 = 0;
+        this.x3 = 0;
+        this.x4 = 0;
+        this.x5 = 0;
+        this.y2 = 0;
+        this.y3 = 0;
+        this.y4 = 0;
+        this.y5 = 0;
     }
+
+    public Particle(double x, double y, double vx, double vy, double fx, double fy, double mass, double radius, String color, double x2, double x3, double x4, double x5, double y2, double y3, double y4, double y5) {
+        this.x = x;
+        this.y = y;
+        this.vx = vx;
+        this.vy = vy;
+        this.fx = fx;
+        this.fy = fy;
+        this.mass = mass;
+        this.radius = radius;
+        this.color = color;
+        this.x2 = x2;
+        this.x3 = x3;
+        this.x4 = x4;
+        this.x5 = x5;
+        this.y2 = y2;
+        this.y3 = y3;
+        this.y4 = y4;
+        this.y5 = y5;
+    }
+
 
     /**
      *  update the invoking particle to simulate it bouncing off a vertical wall.
@@ -109,6 +150,71 @@ public class Particle {
 
     public void setFy(double fy) {
         this.fy = fy;
+    }
+
+    public double getX2() {
+        return x2;
+    }
+
+    public void setX2(double x2) {
+        this.x2 = x2;
+    }
+
+    public double getX3() {
+        return x3;
+    }
+
+    public void setX3(double x3) {
+        this.x3 = x3;
+    }
+
+    public double getX4() {
+        return x4;
+    }
+
+    public void setX4(double x4) {
+        this.x4 = x4;
+    }
+
+    public double getX5() {
+        return x5;
+    }
+
+    public void setX5(double x5) {
+        this.x5 = x5;
+    }
+
+
+    public double getY2() {
+        return y2;
+    }
+
+    public void setY2(double y2) {
+        this.y2 = y2;
+    }
+
+    public double getY3() {
+        return y3;
+    }
+
+    public void setY3(double y3) {
+        this.y3 = y3;
+    }
+
+    public double getY4() {
+        return y4;
+    }
+
+    public void setY4(double y4) {
+        this.y4 = y4;
+    }
+
+    public double getY5() {
+        return y5;
+    }
+
+    public void setY5(double y5) {
+        this.y5 = y5;
     }
 
     @Override

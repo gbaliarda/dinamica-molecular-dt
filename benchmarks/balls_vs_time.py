8f4737b1588717dc82af2e5bc728f035cfa2e873
Ezequiel <eperez@itba.edu.ar>
2023-05-14 22:30:10 -0300

    chore: change bar plots to scatter plots

--- benchmarks/balls_vs_time.py
+++ benchmarks/balls_vs_time.py
@@ -18,90 +18,96 @@ def main() -> None:
   TOTAL_BALLS = 16
   TOTAL_HOLES = 6
 
-  # Avg times when the ball count is 0, 1, 2, ..., 16
-  times = {}
-  # Errors (std) for the previous times
-  errors = {}
-  
-  for i in range(initialPositions):
-    # Update white ball `y` coordinate
-    current_y = whiteBallYRange[0] + step * i
-    config["simulation"]["whiteBallCoords"] = [config["simulation"]["whiteBallCoords"][0], current_y]
+  try:
+    with open('out/times.json', 'r') as file:
+      times = json.load(file)
+    with open('out/errors.json', 'r') as file:
+      errors = json.load(file)
+  except FileNotFoundError:
+    # Avg times when the ball count is 0, 1, 2, ..., 16
+    times = {}
+    # Errors (std) for the previous times
+    errors = {}
+    
+    for i in range(initialPositions):
+      # Update white ball `y` coordinate
+      current_y = whiteBallYRange[0] + step * i
+      config["simulation"]["whiteBallCoords"] = [config["simulation"]["whiteBallCoords"][0], current_y]
 
-    print("Running simulation with white ball at [", config["simulation"]["whiteBallCoords"][0], ", ", current_y, "]")
+      print("Running simulation with white ball at [", config["simulation"]["whiteBallCoords"][0], ", ", current_y, "]")
 
-    with open("config.toml", "w") as f:
-      toml.dump(config, f)
+      with open("config.toml", "w") as f:
+        toml.dump(config, f)
 
-    # Save the times of each round for the current `y`
-    times[current_y] = {}
-    errors[current_y] = {}
+      # Save the times of each round for the current `y`
+      times[current_y] = {}
+      errors[current_y] = {}
 
-    for j in range(rounds):    
-      # Create particles
-      subprocess.run(["python", "generate_particles.py"])
+      for j in range(rounds):    
+        # Create particles
+        subprocess.run(["python", "generate_particles.py"])
 
-      # Run simulation
-      subprocess.run(["java", "-jar", "./target/dinamica-molecular-dt-1.0-SNAPSHOT-jar-with-dependencies.jar"])
+        # Run simulation
+        subprocess.run(["java", "-jar", "./target/dinamica-molecular-dt-1.0-SNAPSHOT-jar-with-dependencies.jar"])
 
-      # Save event times
-      with open(config["files"]["output"], 'r') as file:
-        lines = file.readlines()
+        # Save event times
+        with open(config["files"]["output"], 'r') as file:
+          lines = file.readlines()
 
-      current_balls = TOTAL_BALLS
-      ball_amt_curr_time = current_balls + TOTAL_HOLES
+        current_balls = TOTAL_BALLS
+        ball_amt_curr_time = current_balls + TOTAL_HOLES
 
-      # Initialize an array to contain the times (for all rounds) for each ball amount
-      if j == 0: times[current_y][TOTAL_BALLS] = []
-      times[current_y][TOTAL_BALLS].append(0)
-      
-      for line in lines:
-        data = line.split()
+        # Initialize an array to contain the times (for all rounds) for each ball amount
+        if j == 0: times[current_y][TOTAL_BALLS] = []
+        times[current_y][TOTAL_BALLS].append(0)
+        
+        for line in lines:
+          data = line.split()
 
-        if len(data) == 1:
-          # Discard the holes counted as balls
-          ball_amt_curr_time -= TOTAL_HOLES
+          if len(data) == 1:
+            # Discard the holes counted as balls
+            ball_amt_curr_time -= TOTAL_HOLES
 
-          # If the number of balls has decreased, save the time
-          if ball_amt_curr_time < current_balls:
-            try:
-              times[current_y][ball_amt_curr_time].append(round(time, 3))
-            except KeyError:
-              times[current_y][ball_amt_curr_time] = [round(time, 3)] # initialize the array of times for the current ball amount
+            # If the number of balls has decreased, save the time
+            if ball_amt_curr_time < current_balls:
+              try:
+                times[current_y][ball_amt_curr_time].append(round(time, 3))
+              except KeyError:
+                times[current_y][ball_amt_curr_time] = [round(time, 3)] # initialize the array of times for the current ball amount
 
-            current_balls = ball_amt_curr_time # should be 1 less ball
+              current_balls = ball_amt_curr_time # should be 1 less ball
 
-          time = float(data[0])
-          ball_amt_curr_time = 0
-        else:
-          ball_amt_curr_time += 1
-      
-      if j == 0: times[current_y][0] = []
-      times[current_y][0].append(round(time, 3))
+            time = float(data[0])
+            ball_amt_curr_time = 0
+          else:
+            ball_amt_curr_time += 1
+        
+        if j == 0: times[current_y][0] = []
+        times[current_y][0].append(round(time, 3))
 
-  print("\nRAW VALUES")
-  print(times)
-  print(errors)
+    print("\nRAW VALUES")
+    print(times)
+    print(errors)
 
-  # Take the mean and std of the times for each ball amount
-  for current_y in times.keys():
-    for ball_amt in times[current_y].keys():
-      errors[current_y][ball_amt] = round(np.std(times[current_y][ball_amt]), 3)
+    # Take the mean and std of the times for each ball amount
+    for current_y in times.keys():
+      for ball_amt in times[current_y].keys():
+        errors[current_y][ball_amt] = round(np.std(times[current_y][ball_amt]), 3)
 
-      if (ball_amt == 0): # use standard error for ball_amt = 0 instead of std
-        errors[current_y][ball_amt] /= math.sqrt(len(times[current_y][ball_amt]))
+        if (ball_amt == 0): # use standard error for ball_amt = 0 instead of std
+          errors[current_y][ball_amt] /= math.sqrt(len(times[current_y][ball_amt]))
 
-      times[current_y][ball_amt] = round(np.mean(times[current_y][ball_amt]), 3)
+        times[current_y][ball_amt] = round(np.mean(times[current_y][ball_amt]), 3)
 
-  # Save the mean and error values
-  with open('out/times.json', 'w') as file:
-    json.dump(times, file)
-  with open('out/errors.json', 'w') as file:
-    json.dump(errors, file)
+    # Save the mean and error values
+    with open('out/times.json', 'w') as file:
+      json.dump(times, file)
+    with open('out/errors.json', 'w') as file:
+      json.dump(errors, file)
 
-  print("\nMEAN VALUES")
-  print(times)
-  print(errors)
+    print("\nMEAN VALUES")
+    print(times)
+    print(errors)
 
   # Plot mean time until half of the balls are gone
   plot_half_balls_vs_time(times, errors)
@@ -117,22 +123,23 @@ def plot_half_balls_vs_time(times: dict[float, dict[int, float]], errors: dict[f
 
   # Time until half of the balls are gone
   for y in times.keys():
-    x_values.append(y)
-    print("HALF BALLS AT:", y)
-    y_values.append(times[y][8])
-    stds.append(errors[y][8])
+    x_values.append(round(float(y), 2))
+    y_values.append(times[y]["8"])
+    stds.append(errors[y]["8"])
 
   _, ax = plt.subplots(figsize=(16, 6))
 
-  ax.bar(x_values, y_values, yerr=stds, capsize=5, width=0.5)
+  ax.errorbar(x_values, y_values, yerr=stds, fmt="o", linewidth=1, markersize=0, capsize=3, color='black')
+  ax.scatter(x_values, y_values, s=100)
 
   ax.set_xlabel("Coordenada `y` bola blanca (cm)", fontsize=18)
-  ax.set_ylabel("Tiempo hasta mitad de bolas (s)", fontsize=18)
+  ax.set_ylabel("T8 (s)", fontsize=18)
+
+  ax.set_ylim(bottom=0, top=2.5)
 
   ax.grid()
   plt.tight_layout()
   
-  # plt.show()
   plt.savefig("out/half_balls_times.png")
 
   plt.close()
@@ -145,22 +152,23 @@ def plot_zero_balls_vs_time(times: dict[float, dict[int, float]], errors: dict[f
 
   # Time until all the balls are gone
   for y in times.keys():
-    print("ZERO BALLS AT:", y)
-    x_values.append(y)
-    y_values.append(times[y][0])
-    stds.append(errors[y][0])
+    x_values.append(round(float(y), 2))
+    y_values.append(times[y]["0"])
+    stds.append(errors[y]["0"])
   
   _, ax = plt.subplots(figsize=(16, 6))
 
-  ax.bar(x_values, y_values, yerr=stds, capsize=5, width=0.5)
+  ax.errorbar(x_values, y_values, yerr=stds, fmt="o", linewidth=1, markersize=0, capsize=3, color='black')
+  ax.scatter(x_values, y_values, s=100)
 
   ax.set_xlabel("Coordenada `y` bola blanca (cm)", fontsize=18)
-  ax.set_ylabel("Tiempo hasta finalizar (s)", fontsize=18)
+  ax.set_ylabel("T16 (s)", fontsize=18)
+
+  ax.set_ylim(bottom=0)
 
   ax.grid()
   plt.tight_layout()
   
-  # plt.show()
   plt.savefig("out/zero_balls_times.png")
 
   plt.close()
